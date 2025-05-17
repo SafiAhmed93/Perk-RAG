87cd6406ff203462531a9e0358c91b5fef17b820
junaid-brio <junaid@brio.co.in>
2025-05-17 13:21:52 +0530

    Update cors origin

--- ragapp/app.py
+++ ragapp/app.py
@@ -20,7 +20,7 @@ app.add_middleware(AuthorizationMiddelware)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
